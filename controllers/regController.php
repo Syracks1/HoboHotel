1cafeede2da4aa73bc2feef7f661d9634305dc3b
LUCA classens <lclassens@hotmail.com>
2019-05-15 14:32:23 +0200

    registration
    Registration done, not OOP yet tho

--- controllers/regController.php
+++ controllers/regController.php
@@ -1,6 +1,30 @@
 <?php
 
+  include '../data/DB.php';
   include '../html/register.html';
-  echo 'Hey wassup';
+
+  if(isset($_POST['createaccount']))
+  {
+    $username = $_POST['uname'];
+    $pass = $_POST['pw'];
+    $passr = $_POST['pwr'];
+
+    if(!DB::query('SELECT username FROM user WHERE username=:username', array(':username'=>$username)))
+    {
+      if(strlen($username) >= 3 && strlen($username) <= 32)
+      {
+        if(preg_match('/[a-zA-Z0-9_]+/', $username))
+        {
+          if(strlen($pass) >= 6 && strlen($pass) <= 60)
+          {
+            if($pass == $passr)
+            {
+              DB::query('INSERT INTO user VALUES (null, :username, :password, 1)', array(':username'=>$username, ':password'=>password_hash($pass, PASSWORD_BCRYPT)));
+            }
+          }
+        }
+      }
+    }
+  }
 
 ?>
