71e04f9cb3b9e716ff36112e9554b459b5ae7bc6
LUCA classens <lclassens@hotmail.com>
2019-05-27 10:01:25 +0200

    Login
    Made it working. I'm an idiot. I know. I forgot to change the name of the Submit button

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -13,13 +13,13 @@
 
     if(DB::query('SELECT username FROM user WHERE username=:username', array(':username'=>$username)))
     {
-      if(password_verify($pass, DB::query('SELECT password FROM user WHERE username=:username', array(':username'=>$username))[0]['password']))
+      if (password_verify($pass, DB::query('SELECT password FROM user WHERE username=:username', array(':username'=>$username))[0]['password']))
       {
         $cstrong = TRUE;
         $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
 
         $user->setUserID(DB::query('SELECT ID FROM user WHERE username=:username', array(':username'=>$username))[0]['ID']);
-        DB::query('INSERT INTO tokens VALUES(null, :token, :ID)', array(':token'=>sha1($token), ':ID'=>$user->getUserID()));
+        DB::query('INSERT INTO tokens VALUES(null, :ID, :token)', array(':token'=>sha1($token), ':ID'=>$user->getUserID()));
 
         setcookie('SNID', $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, TRUE);
         setcookie('SNID_', '1', time() + 60 * 60 * 24 * 3, '/', NULL, NULL, TRUE);
@@ -28,6 +28,5 @@
     }
     else{echo 'User does not Exist';}
   }
-
 
 ?>
