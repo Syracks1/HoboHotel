42717250807dbccfacee5d93fd0a488fbde81729
LUCA classens <lclassens@hotmail.com>
2019-06-03 11:50:23 +0200

    Some HTML Stuff
    Changed some HTML stuff

--- views/listings.php
+++ views/listings.php
@@ -26,7 +26,7 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="#">Navbar</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item active"><a class="nav-link" href="../index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="../controllers/loginController.php">Login</a></li>
                 <li class="nav-item"><a class="nav-link" href="../controllers/regController.php">Register</a></li>
                 <li class="nav-item"><a class="nav-link active" href="./views/listings.php">Listings</a></li>
